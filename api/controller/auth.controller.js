b715008a0f20792d406f521bc4281879ae5a9762
Pratyasha <pratyashapanda1510@gmail.com>
2024-06-21 19:07:37 +0530

    create get user listing api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -20,9 +20,12 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found!"));
+
     const isValidPassword = bcryptjs.compareSync(password, validUser.password);
     if (!isValidPassword) return next(errorHandler(401, "Wrong Credentials!"));
+
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    
     const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
