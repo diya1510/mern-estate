a32c043f592009b9ac1d47cf3a35e093876b4524
Pratyasha <pratyashapanda1510@gmail.com>
2024-07-03 12:27:31 +0530

    ..

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -25,7 +25,7 @@ export const signin = async (req, res, next) => {
     if (!isValidPassword) return next(errorHandler(401, "Wrong Credentials!"));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    
+
     const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
@@ -53,7 +53,9 @@ export const google = async (req, res, next) => {
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
 
       const newUser = new User({
-        username: req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4),
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
         // req.body.username.split(' ').join('').toLowerCase() +
         //   Math.random().toString(36).slice(-4),
 
@@ -74,12 +76,11 @@ export const google = async (req, res, next) => {
   }
 };
 
-
 export const signOut = async (req, res, next) => {
   try {
-    res.clearCookie('access_token');
-    res.status(200).json('User has been logged out!');
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
   } catch (error) {
     next(error);
   }
-}
+};
