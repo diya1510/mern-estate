e06c9f7957218b490e128cff9db224da970456ec
Pratyasha <pratyashapanda1510@gmail.com>
2024-07-07 21:11:56 +0530

    add show more listing functionality

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -4,11 +4,13 @@ import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing }) {
   return (
-    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px] '>
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px] ">
       <Link to={`/listing/${listing._id}`}>
         <img
-          src={listing.imageUrls[0] ||             'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
-        }
+          src={
+            listing.imageUrls[0] ||
+            "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg"
+          }
           alt="listing cover "
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
@@ -26,19 +28,20 @@ export default function ListingItem({ listing }) {
             {listing.description}
           </p>
           <p className="text-slate-500 mt-2 font-semibold flex items-center ">
-            ${listing.offer
+            $
+            {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
               : listing.regularPrice.toLocaleString("en-US")}
             {listing.type === "rent" && " / month"}
           </p>
           <div className="text-slate-700 flex gap-4">
             <div className="font-bold text-xs ">
-            {listing.bedrooms > 1
+              {listing.bedrooms > 1
                 ? `${listing.bedrooms} beds `
                 : `${listing.bedrooms} bed `}
             </div>
             <div className="font-bold text-xs ">
-            {listing.bathrooms > 1
+              {listing.bathrooms > 1
                 ? `${listing.bathrooms} baths `
                 : `${listing.bathrooms} bath `}
             </div>
