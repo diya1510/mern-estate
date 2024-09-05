475d4d5fdf432ee5c948769b42eadbc2858bb115
Pratyasha <pratyashapanda1510@gmail.com>
2024-09-05 20:20:16 +0530

    about changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,26 +5,25 @@ export default function About() {
     <div className="py-20 px-4 max-w-6xl mx-auto ">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">About Panda Estate</h1>
       <p className="mb-4 text-slate-700">
-        Sahand Estate is a leading real estate agency that specializes in
-        helping clients buy, sell, and rent properties in the most desirable
-        neighborhoods. Our team of experienced agents is dedicated to providing
-        exceptional service and making the buying and selling process as smooth
-        as possible.
+      Welcome to Panda Estate, where luxury meets comfort. It is a leading estate 
+      agency that specializes in helping clients buy, sell, and rent properties in the most 
+      desirable neighborhoods. Our team of experienced real estate professionals is dedicated
+       to providing personalized service and guidance throughout the home buying or selling
+        process as smooth as possible.
       </p>
 
       <p className="mb-4 text-slate-700">
-        Our mission is to help our clients achieve their real estate goals by
-        providing expert advice, personalized service, and a deep understanding
-        of the local market. Whether you are looking to buy, sell, or rent a
-        property, we are here to help you every step of the way.
+      Our mission is to provide discerning clients with exceptional properties that 
+      cater to their unique needs and desires. Whether you're looking for a serene retreat,
+       a modern city residence, or a grand estate, Panda Estate offers a curated selection of 
+       homes that epitomize elegance and style.
       </p>
 
       <p className="mb-4 text-slate-700">
-        Our team of agents has a wealth of experience and knowledge in the real
-        estate industry, and we are committed to providing the highest level of
-        service to our clients. We believe that buying or selling a property
-        should be an exciting and rewarding experience, and we are dedicated to
-        making that a reality for each and every one of our clients.
+      At Panda Estate, we believe that buying or selling a home is a significant life event,
+       and we strive to make the process as stress-free and enjoyable as possible. Our team of 
+       experts is committed to providing the highest level of service and support, from start to
+        finish.
       </p>
     </div>
   );
